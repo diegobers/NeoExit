0a4101f4e835242a4ad27b2c4af711f8c88f4161
Diego Bernardi <diego-bs@hotmail.com>
2022-01-24 01:20:01 -0300

    settings to heroku

--- main/settings.py
+++ main/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import django_heroku 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,3 +141,6 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+django_heroku.settings(locals())
