6f11620cc67ec6af542304c3ca82f9704dc2b7f8
Diego Bernardi <diego-bs@hotmail.com>
2022-01-24 00:50:34 -0300

    CRUD ok, add filtros

---
+++ investir/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ofertar.models import Oferta
+
+
+STATUS = [
+    ('Solicitado','Solicitado'),
+    ('Em Andamento','Em Andamento'),
+    ('Aprovado','Aprovado'),
+    ('Finalizado','Finalizado'),
+]
+
+
+class Investimento(models.Model):
+    cliente = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) 
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True)
+    oferta = models.ForeignKey(Oferta, null=True, on_delete=models.SET_NULL, blank=True )
+    valor = models.DecimalField(null=True, max_digits=9, decimal_places=2)    
+    status = models.CharField(max_length=200, null=True, choices=STATUS, default='Solicitado')
+  
+    class Meta:
+        ordering = ['valor']   
+
+  
+    
+ 
